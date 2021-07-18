c4279d471a74770832735542d41b3ac32ab5e9ab
woori3104 <sizuku11261@gmail.com>
2021-07-19 03:15:33 +0900

    graphql Query, Types .Net Core

---
+++ Source/GraphQlProject/Query/productQuery.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using GraphQL;
+using GraphQL.Types;
+using GraphQlProject.Interfaces;
+using GraphQlProject.Type;
+
+namespace GraphQlProject.Query
+{
+    public class productQuery :ObjectGraphType
+    {
+        public productQuery(IProduct productService)
+        {
+            Field<ListGraphType<ProductType>>("products", resolve: context => { return productService.GetAllProducts(); });
+            Field<ProductType>("products",arguments:
+                new QueryArguments(new QueryArgument<IntGraphType> { Name = "id"}),
+
+                resolve: context =>
+                { return productService.GetProductById(context.GetArgument<int>("id"));
+                });
+        }
+        
+    }
+}
---
+++ Source/GraphQlProject/Type/ProductType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using GraphQL.Types;
+using GraphQlProject.Models;
+
+namespace GraphQlProject.Type
+{
+    public class ProductType : ObjectGraphType<Product>
+    {
+        public ProductType()
+        {
+            Field(p => p.Id);
+            Field(p => p.Name);
+            Field(p => p.Price);
+        }
+    }
+}
